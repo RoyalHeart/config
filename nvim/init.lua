3a187ed6e37d1c5d70bb287c960cd9812f7f41b7
Tam Thai <hoangtam3062002@gmail.com>
2022-09-18 10:32:40 +0700

    Reorder nvim option

--- nvim/init.lua
+++ nvim/init.lua
@@ -82,28 +82,27 @@ vim.cmd [[colorscheme catppuccin]]
 
 -- Basic config
 vim.opt.autoindent = true--indent the same in new line
-vim.opt.list = true--show the white spaces
-vim.opt.ttyfast = true--fast scrolling
-vim.opt.showmatch = true--show matching
-vim.opt.ignorecase = true--case insensitive
+vim.opt.cc='80'--column border
+vim.opt.clipboard='unnamedplus'--using system clipboard
 vim.opt.hlsearch = true--highlight search
+vim.opt.ignorecase = true--case insensitive
 vim.opt.incsearch = true--incremental search
+vim.opt.list = true--show the white spaces
+vim.opt.listchars = {space = '·', tab = '>>'}--show how to render space,tab
+vim.opt.mouse='a'--enable click with mouse
 vim.opt.number = true--show line number
 vim.opt.relativenumber = true--show relative line number
-vim.opt.mouse='a'--enable click with mouse
-vim.opt.listchars = {space = '·', tab = '>>'}--show how to render space,tab
-vim.opt.tabstop=4--spaces after tab
-vim.opt.softtabstop=4--see multiples spaces with tab
+vim.opt.scrolloff=10--auto-scroll when n lines from border
+vim.opt.showmatch = true--show matching
 vim.opt.shiftwidth=4--width for autoindent
+vim.opt.softtabstop=4--see multiples spaces with tab
+vim.opt.tabstop=4--spaces after tab
+vim.opt.textwidth=80--textwidth max
+vim.opt.timeoutlen=400--timeout between keys
+vim.opt.ttyfast = true--fast scrolling
 vim.opt.wildmenu=true
 vim.opt.wildmode={'list','longest','full'}--bash-like tab completion
-vim.opt.cc='80'--column border
-vim.opt.textwidth=80--textwidth max
-vim.opt.scrolloff=10--auto-scroll when n lines from border
-vim.opt.clipboard='unnamedplus'--using system clipboard
-vim.opt.timeoutlen=400--timeout between keys
 vim.g.mapleader = " "--leader key is space
-
 --Remap keys:
 -- normal mode:
 nmap('<Space>', '<Nop>') -- make leader key no weird
--- nvim/settings/airline.vim
+++ nvim/settings/airline.vim
@@ -1,5 +1,5 @@
 let g:airline_powerline_fonts = 1                       " Enable font for status bar
-let g:airline_theme='onedark'                           " Theme OneDark
+let g:airline_theme='bubblegum'                           " Theme OneDark
 
 let g:airline#extensions#tabline#enabled = 1            " Enable Tab bar
 let g:airline#extensions#tabline#left_sep = ' '         " Enable Tab seperator 
